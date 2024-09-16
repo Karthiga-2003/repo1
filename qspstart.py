14022bb55a25f72cd1d34a262bc6f2382557e4d9
Karthiga-2003 <146521019+Karthiga-2003@users.noreply.github.com>
2024-09-16 10:31:22 +0530

    Update qspstart.py

--- qspstart.py
+++ qspstart.py
@@ -2,4 +2,6 @@ print("Hello World!!")
 age=int(input("Enter age:"))
 print(f"your age is {age}")
 if age < 18:
-  print("you are not eligible")
+  print("you are not eligible for voting")
+else:
+  print("you are eligible for voting")
